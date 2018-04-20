b3bb88c8cc5371f208119aa57d5a6191f7ef4516
Mori <panelmori@gmail.com>
2018-04-20 16:02:09 +0700

    xxx

--- src/app/main/userinfo/userinfo-form/userinfo-form.component.ts
+++ src/app/main/userinfo/userinfo-form/userinfo-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from '../../../baseComponent';
+import { Http, Headers, RequestOptions} from '@angular/http';
 
 @Component({
   selector: 'app-userinfo-form',
@@ -12,8 +13,8 @@ export class UserinfoFormComponent extends BaseComponent implements OnInit {
   public lastname: string;
   public introduce: string;
   public titlename: string;
-
-  constructor() { 
+  private pathUpdate = this._CONF.SERVER_API + "";
+  constructor(private http: Http) { 
     super();
     
     this.firstname = this.user.firstname;
@@ -26,4 +27,20 @@ export class UserinfoFormComponent extends BaseComponent implements OnInit {
     console.log(this.user);
   }
 
+  onSave(){
+    let headobj = { 'Content-Type': 'application/json' };
+    let headers = new Headers(headobj);
+    let options = new RequestOptions({ headers: headers, method: "put" });
+    let parameter = {};
+    this.http.put(this.pathUpdate, parameter, options)
+    .map(res => res.json())
+    .subscribe(
+    data => this.onCompleted(data),
+    err => console.log('err', err),
+    () => console.log('Fetching complete for Server Api.')
+    );
+  }
+  onCompleted(data){
+    console.log('data', data);
+  }
 }
